d2990c45b1f786a67c65092e36934a40d51a4bc5
DoubleH106 <quanghuong106@gmail.com>
2024-05-11 00:16:45 +0700

    commit show comment and comment

--- lib/Controller/HomeController.dart
+++ lib/Controller/HomeController.dart
@@ -52,4 +52,40 @@ class HomeController{
       throw Exception('Failed to load data deleteItem');
     }
   }
+  static Future<dynamic> addcomment(int Accid,int HomeId,String comment) async{
+    final response = await http.post(
+      Uri.parse('http://localhost:7239/api/Home/Comments'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'accId': 1,
+        'homeId':HomeId,
+        'comment':comment
+      }),
+    );
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load data deleteItem');
+    }
+  }
+  static Future<dynamic> listComments(int Accid,int HomeId,String comment) async{
+    final response = await http.post(
+      Uri.parse('http://localhost:7239/api/Home/listComments'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'accId': 0,
+        'homeId':HomeId,
+        'comment':""
+      }),
+    );
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load data deleteItem');
+    }
+  }
 }
--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -13,10 +13,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class HomeScreenState extends State<HomeScreen> {
-  List<dynamic> Comments = [];
+  List<dynamic> Commentsssss = [];
   List<dynamic> News = [];
   bool ShowComment = false;
-
+  bool send = false;
+  String NameUser = "";
+  String Avatars ="";
+  String cmt="";
+  var idpost = 0;
+  TextEditingController commentsController = TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   void initState() {
     super.initState();
@@ -38,8 +43,32 @@ class HomeScreenState extends State<HomeScreen> {
     final data = await HomeController.likepost(accId, homeId);
   }
 
+  void listComments(int homeId) async {
+    final data1 = await HomeController.listComments(0, homeId, "");
+   try{
+     setState(() {
+       Commentsssss = data1;
+       // NameUser = data['acc'];
+       // Avatars = data['avatars'];
+     });
+   }catch(e) {
+     print('Error: fetchData $e');
+   }
+
+  }
+
   void offlikepost(int accId, int homeId) async {
     final data = await HomeController.offlikepost(accId, homeId);
+  }
+
+  void sendcomment() async {
+    String _comments = commentsController.text;
+    final data = await HomeController.addcomment(1, idpost, _comments);
+    if (data["status"] == true) {
+      commentsController.text = "";
+    } else {
+      commentsController.text = "";
+    }
   }
 
   @override
@@ -59,6 +88,7 @@ class HomeScreenState extends State<HomeScreen> {
                   itemCount: News.length,
                   itemBuilder: (context, index) {
                     var item = News[index];
+                    // Comments = item['listComment'];
                     return Column(
                       children: [
                         Row(
@@ -104,14 +134,14 @@ class HomeScreenState extends State<HomeScreen> {
                                 fit: BoxFit.cover,
                               )),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 10,
                         ),
                         Text(
                           item["title"],
                           style: TextStyle(fontSize: 18),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           height: 10,
                         ),
                         Row(
@@ -152,8 +182,11 @@ class HomeScreenState extends State<HomeScreen> {
                             ),
                             GestureDetector(
                               onTap: () {
+                                listComments(item['homeId']);
                                 setState(() {
                                   ShowComment = true;
+                                  idpost = item['homeId'];
+                                  // print('Id bài viết $idpost');
                                 });
                               },
                               child: const Icon(
@@ -166,12 +199,6 @@ class HomeScreenState extends State<HomeScreen> {
                             const SizedBox(
                               width: 10,
                             ),
-                            // const Icon(
-                            //   Icons.share,
-                            //   color:
-                            //       Colors.grey, // IconData của biểu tượng trái tim
-                            //   size: 40, // Kích thước của biểu tượng
-                            // )
                           ],
                         ),
                         const SizedBox(
@@ -184,11 +211,12 @@ class HomeScreenState extends State<HomeScreen> {
               ),
             ),
             if (ShowComment == true)
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.grey.withOpacity(0.5), // Màu xám với độ trong suốt là 0.5
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey
+                      .withOpacity(0.5), // Màu xám với độ trong suốt là 0.5
+                ),
               ),
-            ),
             if (ShowComment == true)
               Center(
                 child: Padding(
@@ -196,61 +224,165 @@ class HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     width: MediaQuery.of(context).size.width,
                     height: MediaQuery.of(context).size.height * 0.8,
-                    decoration: BoxDecoration(color: Colors.white,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
                       borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
-                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Row(
-                          children: [
-                            GestureDetector(
-                                onTap: () {
-                                  setState(() {
-                                    ShowComment = false;
-                                  });
-                                },
-                                child: Icon(Icons.reply_all)),
-                            const SizedBox(
-                              width: 50,
-                            ),
-                            const Text("Chưa có bình luận nào."),
-                            const SizedBox(
-                              height: 50,
-                            ),
-                          ],
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.9,
+                          child: Row(
+                            children: [
+                              GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      ShowComment = false;
+                                    });
+                                  },
+                                  child: const Icon(Icons.reply_all)),
+
+                              const SizedBox(
+                                width: 50,
+                              ),
+                              if (Commentsssss.length > 0)
+                              Text('Có ${Commentsssss.length} bình luận',style: TextStyle(fontSize: 20),),
+                              if (Commentsssss.length ==0)
+                                const Text("Chưa có bình luận nào.",style: TextStyle(fontSize: 20),),
+                              const SizedBox(
+                                height: 50,
+                              ),
+                            ],
+                          ),
                         ),
-                        // const SizedBox(
-                        //   height: 50,
-                        // ),
-                        const Column(
-                          children: [
-                            Icon(
-                              Icons.comment_outlined,
-                              size: 150,
-                            ),
-                            Text("Hãy là người đầu tiên bình luận."),
-                          ],
+                        if (Commentsssss.length ==0)
+                          const Column(
+                            children: [
+                              Icon(
+                                Icons.comment_outlined,
+                                size: 150,
+                              ),
+                              Text("Hãy là người đầu tiên bình luận."),
+                            ],
+                          ),
+                        if (Commentsssss.length > 0)
+                        Expanded(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width * 0.9,
+                            height:100,
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                                itemCount: Commentsssss.length ,
+                                itemBuilder: (context, index) {
+                                  var item = Commentsssss[index];
+                                  return Column(
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Container(
+                                            width: 50,
+                                            height: 50,
+                                            decoration: BoxDecoration(
+                                                shape: BoxShape.circle,
+                                                image: DecorationImage(
+                                                    image: AssetImage(item['avatars']))),
+                                          ),
+                                          SizedBox(width: 20,),
+                                          Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(item['acc'],style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                                              Text(item['comments'],style: TextStyle(fontSize: 20))
+                                            ],
+                                          ),
+                                          Text('Trả lời'),
+                                        ],
+                                      ),
+                                      SizedBox(height:20),
+                                    ],
+                                  );
+                                },),
+                          ),
                         ),
                         Row(
                           children: [
                             Expanded(
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                                ),
-
-                                child: TextField(
-                                  // onTap: () {
-                                  //   FocusScope.of(context).requestFocus(FocusNode()); // Yêu cầu focus khi người dùng nhấp vào TextField
-                                  // },
-                                  decoration: const InputDecoration(
-                                      hintText: 'Viết bình luận...'),
+                              child: Form(
+                                key: _formKey,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10)),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Expanded(
+                                        child: Padding(
+                                          padding:
+                                              const EdgeInsets.only(left: 20),
+                                          child: TextFormField(
+                                            onChanged: (text) {
+                                              if (text != "") {
+                                                setState(() {
+                                                  send = true;
+                                                });
+                                              } else {
+                                                setState(() {
+                                                  send = false;
+                                                });
+                                              }
+                                            },
+                                            controller: commentsController,
+                                            style:
+                                                TextStyle(color: Colors.black),
+                                            validator: (value) {
+                                              if (value == null ||
+                                                  value.isEmpty) {
+                                                return 'Please enter some text';
+                                              }
+                                              return null;
+                                            },
+                                            decoration: const InputDecoration(
+                                              hintText: 'Viết bình luận...',
+                                              errorStyle: TextStyle(
+                                                  color: Colors.black),
+                                              labelStyle: TextStyle(
+                                                  color: Colors.black),
+                                              hintStyle: TextStyle(
+                                                  color: Colors.black),
+                                              border: InputBorder.none,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                      if (send == true)
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(right: 20),
+                                          child: GestureDetector(
+                                              // Cần Sửa
+                                              onTap: () {
+                                                sendcomment();
+                                              },
+                                              child: const Icon(Icons.send,
+                                                  color: Colors.green)),
+                                        ),
+                                      if (send == false)
+                                        const Padding(
+                                          padding: EdgeInsets.only(right: 20),
+                                          child: Icon(Icons.send,
+                                              color: Colors.white),
+                                        ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            )
+                            ),
                           ],
                         ),
                       ],
